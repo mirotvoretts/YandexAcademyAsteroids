f59e89317a5820dc5c02804a58c67380fe2c152f
mirotvoretts <panoffilya@gmail.com>
2023-02-28 17:26:17 +0500

    FourthLesson

---
+++ Assets/Sources/Model/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace Asteroids.Model
+{
+    public static class Config
+    {
+        public const float NloSpeed = 0.075f;
+        public const float AsteroidSpeed = 0.075f;
+        public const float LaserCooldown = 3f;
+    }
+}
---
+++ Assets/Sources/Model/Weapon/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace Asteroids.Model
+{
+    public abstract class Bullet
+    {
+        public readonly float Lifetime;
+        public readonly float Speed;
+
+        protected Bullet(float lifetime, float speed)
+        {
+            Lifetime = lifetime;
+            Speed = speed;
+        }
+    }
+
+    public class LaserGunBullet : Bullet
+    {
+        public LaserGunBullet() : base(0.5F, 0F) { }
+    }
+
+    public class DefaultBullet : Bullet
+    {
+        public DefaultBullet() : base(10F, 0.6F) { }
+    }
+}
